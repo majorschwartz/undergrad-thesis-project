db27db4cbf54966c64bb8e22293829bf8465abeb
Major <107882188+qubitflip@users.noreply.github.com>
2024-08-30 18:19:16 -0400

    Prepping useContext

--- client/src/components/hooks/useContext.js
+++ client/src/components/hooks/useContext.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const useContext = () => {
-	return (
-		<div>
-			
-		</div>
-	);
+	const [models, setModels] = useState([])
+	const [selectedModels, setSelectedModels] = useState([])
+
+	useEffect(() => {
+		fetchModels()
+	}, [])
+
+	const fetchModels = async () => {
+		const response = await fetch('/api/models')
+		const data = await response.json()
+		setModels(data)
+	}
+
+	return { models, selectedModels, setSelectedModels }
 }
 
 export default useContext;
